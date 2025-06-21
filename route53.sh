d509cb09cd40d6ec40bdba12d6e0a2b6c6c213b6
Nagashankar <nagashankar0848@gmail.com>
2025-06-21 12:50:00 +0530

    functions

--- route53.sh
+++ route53.sh
@@ -16,12 +16,12 @@ aws route53 change-resource-record-sets \
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                 "Value"         : "'$IP'"
+                "Value"         : "'$IP'"
             }]
         }
         }]
-    } &> /dev/null
+    }' &> /dev/null
